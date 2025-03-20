fe19e5b02d862227b253f51d132e3c16ce6fdf24
JonnyOThan <jonnyothan@gmail.com>
2025-03-20 11:12:59 -0400

    WIP: adopt KSPBuildTools -main project builds successfully, but: -I haven't figured out exactly what all the python tools are doing yet -This relies on having the underlying dlls all submitted to gamedata -haven't looked at versioning, etc yet

--- KSPBurst/Properties/AssemblyInfo.cs
+++ KSPBurst/Properties/AssemblyInfo.cs
@@ -1,15 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("KSPBurst")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KSPBurst")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,17 +10,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("62AF95D6-FE71-485C-92C2-3F6B79F26B28")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.5.1")]
-[assembly: AssemblyFileVersion("1.5.5.1")]
 [assembly: KSPAssembly("KSPBurst", 1, 5, 5)]
